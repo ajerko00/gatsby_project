ab0756ce91a395b328ad49b883e24d3c04681349
ajerko00 <48961867+ajerko00@users.noreply.github.com>
2019-12-10 20:36:17 +0100

    commit_5

---
+++ .cache/async-requires.js
@@ -0,0 +1,15 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-drivers-js": () => import("../src/pages/Drivers.js" /* webpackChunkName: "component---src-pages-drivers-js" */),
+  "component---src-pages-index-js": () => import("../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-login-js": () => import("../src/pages/Login.js" /* webpackChunkName: "component---src-pages-login-js" */),
+  "component---src-pages-page-2-js": () => import("../src/pages/page-2.js" /* webpackChunkName: "component---src-pages-page-2-js" */),
+  "component---src-pages-schedule-js": () => import("../src/pages/Schedule.js" /* webpackChunkName: "component---src-pages-schedule-js" */),
+  "component---src-pages-standings-js": () => import("../src/pages/Standings.js" /* webpackChunkName: "component---src-pages-standings-js" */),
+  "component---src-pages-teams-js": () => import("../src/pages/Teams.js" /* webpackChunkName: "component---src-pages-teams-js" */)
+}
+
---
+++ .cache/sync-requires.js
@@ -0,0 +1,18 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\Antonio\\gatsby-project\\.cache\\dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\Antonio\\gatsby-project\\src\\pages\\404.js"))),
+  "component---src-pages-drivers-js": hot(preferDefault(require("C:\\Users\\Antonio\\gatsby-project\\src\\pages\\Drivers.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\Antonio\\gatsby-project\\src\\pages\\index.js"))),
+  "component---src-pages-login-js": hot(preferDefault(require("C:\\Users\\Antonio\\gatsby-project\\src\\pages\\Login.js"))),
+  "component---src-pages-page-2-js": hot(preferDefault(require("C:\\Users\\Antonio\\gatsby-project\\src\\pages\\page-2.js"))),
+  "component---src-pages-schedule-js": hot(preferDefault(require("C:\\Users\\Antonio\\gatsby-project\\src\\pages\\Schedule.js"))),
+  "component---src-pages-standings-js": hot(preferDefault(require("C:\\Users\\Antonio\\gatsby-project\\src\\pages\\Standings.js"))),
+  "component---src-pages-teams-js": hot(preferDefault(require("C:\\Users\\Antonio\\gatsby-project\\src\\pages\\Teams.js")))
+}
+
