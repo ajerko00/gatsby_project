63e07e16001d97103e7959e45464f35313c1c922
Your Name <you@example.com>
2020-01-08 12:48:37 +0100

    commit_10

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -1,11 +1,11 @@
 var plugins = [{
-      plugin: require('C:/Users/Antonio/gatsby-project/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
+      plugin: require('D:/gatsby-project/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
       options: {"plugins":[]},
     },{
-      plugin: require('C:/Users/Antonio/gatsby-project/node_modules/@debiki/gatsby-plugin-talkyard/gatsby-ssr'),
+      plugin: require('D:/gatsby-project/node_modules/@debiki/gatsby-plugin-talkyard/gatsby-ssr'),
       options: {"plugins":[],"talkyardServerUrl":"https://comments-demo.talkyard.io"},
     },{
-      plugin: require('C:/Users/Antonio/gatsby-project/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
+      plugin: require('D:/gatsby-project/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
       options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/images/a/f1-logo.png"},
     }]
 // During bootstrap, we write requires at top of this file which looks like:
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,15 +5,15 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---src-templates-post-js": hot(preferDefault(require("C:\\Users\\Antonio\\gatsby-project\\src\\templates\\post.js"))),
-  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\Antonio\\gatsby-project\\.cache\\dev-404-page.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\Antonio\\gatsby-project\\src\\pages\\404.js"))),
-  "component---src-pages-drivers-js": hot(preferDefault(require("C:\\Users\\Antonio\\gatsby-project\\src\\pages\\Drivers.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\Antonio\\gatsby-project\\src\\pages\\index.js"))),
-  "component---src-pages-login-js": hot(preferDefault(require("C:\\Users\\Antonio\\gatsby-project\\src\\pages\\Login.js"))),
-  "component---src-pages-page-2-js": hot(preferDefault(require("C:\\Users\\Antonio\\gatsby-project\\src\\pages\\page-2.js"))),
-  "component---src-pages-schedule-js": hot(preferDefault(require("C:\\Users\\Antonio\\gatsby-project\\src\\pages\\Schedule.js"))),
-  "component---src-pages-standings-js": hot(preferDefault(require("C:\\Users\\Antonio\\gatsby-project\\src\\pages\\Standings.js"))),
-  "component---src-pages-teams-js": hot(preferDefault(require("C:\\Users\\Antonio\\gatsby-project\\src\\pages\\Teams.js")))
+  "component---src-templates-post-js": hot(preferDefault(require("D:\\gatsby-project\\src\\templates\\post.js"))),
+  "component---cache-dev-404-page-js": hot(preferDefault(require("D:\\gatsby-project\\.cache\\dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("D:\\gatsby-project\\src\\pages\\404.js"))),
+  "component---src-pages-drivers-js": hot(preferDefault(require("D:\\gatsby-project\\src\\pages\\Drivers.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("D:\\gatsby-project\\src\\pages\\index.js"))),
+  "component---src-pages-login-js": hot(preferDefault(require("D:\\gatsby-project\\src\\pages\\Login.js"))),
+  "component---src-pages-page-2-js": hot(preferDefault(require("D:\\gatsby-project\\src\\pages\\page-2.js"))),
+  "component---src-pages-schedule-js": hot(preferDefault(require("D:\\gatsby-project\\src\\pages\\Schedule.js"))),
+  "component---src-pages-standings-js": hot(preferDefault(require("D:\\gatsby-project\\src\\pages\\Standings.js"))),
+  "component---src-pages-teams-js": hot(preferDefault(require("D:\\gatsby-project\\src\\pages\\Teams.js")))
 }
 
