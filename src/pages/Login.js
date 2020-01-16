8732f5324a056a77064bb0a9977e1389c05ae82b
ajerko00 <48961867+ajerko00@users.noreply.github.com>
2020-01-16 16:51:17 +0100

    login_bugfix

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,65 @@
 import React from "react"
-import Login from "../components/login"
+import { navigate } from "gatsby"
+import { handleLogin, isLoggedIn } from "../services/auth"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import "../styles/login.css"
 
-const log = ({ data }) => {
-  return <Login />
+class Login extends React.Component {
+  state = {
+    username: ``,
+    password: ``,
+  }
+  handleUpdate = event => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    })
+  }
+  handleSubmit = event => {
+    event.preventDefault()
+    handleLogin(this.state)
+  }
+  render() {
+    if (isLoggedIn()) {
+      navigate(`/app/profile`)
+    }
+    return (
+      <>
+        <SEO title="Login" />
+
+        <Layout />
+
+        <div className="LoginForm">
+          <form
+            method="post"
+            onSubmit={event => {
+              this.handleSubmit(event)
+              navigate(`/app/profile`)
+            }}
+          >
+            <label className="userLogin">
+              Username
+              <input
+                type="text"
+                className="username"
+                name="username"
+                onChange={this.handleUpdate}
+              />
+            </label>
+            <label>
+              Password
+              <input
+                type="password"
+                name="password"
+                className="password"
+                onChange={this.handleUpdate}
+              />
+            </label>
+            <input type="submit" value="Log In" className="login" />
+          </form>
+        </div>
+      </>
+    )
+  }
 }
-export default log
+export default Login
--- src/pages/app.js
+++ src/pages/app.js
@@ -3,7 +3,7 @@ import { Router } from "@reach/router"
 import Layout from "../components/layout"
 import PrivateRoute from "../components/privateRoute"
 import Profile from "../components/profile"
-import Login from "../components/login"
+import Login from "./login"
 const App = () => (
   <Layout>
     <Router>
