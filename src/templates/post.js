2b256282f8e95cc7fdf00f69afda146a87c60d20
ajerko00 <48961867+ajerko00@users.noreply.github.com>
2020-02-10 18:34:15 +0100

    added articles commentBox

--- src/templates/post.js
+++ src/templates/post.js
@@ -3,6 +3,29 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import "../styles/post.css"
 import Footer from "../components/footer"
+import { isLoggedIn } from "../services/auth"
+
+class JustComments extends React.Component {
+  constructor(...args) {
+    super(...args)
+    this.ref = React.createRef()
+  }
+  render() {
+    return (
+      <div
+        ref={this.ref}
+        className="just-comments"
+        data-apikey="7ef857fd-adc0-435e-9478-23fde1808f88"
+      />
+    )
+  }
+  componentDidMount() {
+    const s = document.createElement("script")
+    s.src = "//just-comments.com/w.js"
+    s.setAttribute("data-timestamp", +new Date())
+    this.ref.current.appendChild(s)
+  }
+}
 
 export default function Template({ data }) {
   const { markdownRemark: post } = data
@@ -16,6 +39,9 @@ export default function Template({ data }) {
         className="sadrzaj"
         dangerouslySetInnerHTML={{ __html: post.html }}
       />
+
+      {isLoggedIn() ? <JustComments /> : <div />}
+
       <Footer />
     </div>
   )
