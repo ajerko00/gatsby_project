1c637e523ec9fe1db0524aac42b4d6a06423f762
ajerko00 <48961867+ajerko00@users.noreply.github.com>
2020-02-13 16:13:09 +0100

    drivers css v2

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -2,9 +2,6 @@ var plugins = [{
       plugin: require('C:/Users/Antonio/gatsby-project/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
       options: {"plugins":[]},
     },{
-      plugin: require('C:/Users/Antonio/gatsby-project/node_modules/@debiki/gatsby-plugin-talkyard/gatsby-ssr'),
-      options: {"plugins":[],"talkyardServerUrl":"https://comments-demo.talkyard.io"},
-    },{
       plugin: require('C:/Users/Antonio/gatsby-project/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
       options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/images/a/f1-logo.png"},
     }]
--- src/pages/Drivers.js
+++ src/pages/Drivers.js
@@ -28,174 +28,168 @@ const Drivers = ({ data }) => {
       <SEO title="Drivers" />
 
       <div id="grid-container-drivers">
-        <div id="top3">
-          <Link to="/Hamilton" className="grid-item-drivers">
-            <Img fluid={data.Hamilton.childImageSharp.fluid} />
-            <p>Lewis Hamilton </p>
-          </Link>
-
-          <Link to="/Bottas" className="grid-item-drivers">
-            <Img fluid={data.Bottas.childImageSharp.fluid} />
-            <p>Valtteri Bottas</p>
-          </Link>
-          <Link to="/Vettel" className="grid-item-drivers">
-            <Img fluid={data.Vettel.childImageSharp.fluid} />
-            <p>Sebastian Vettel</p>
-          </Link>
-          <Link to="/Leclerc" className="grid-item-drivers">
-            <Img fluid={data.Leclerc.childImageSharp.fluid} />
-            <p>Charles Leclerc</p>
-          </Link>
-          <Link to="/Verstappen" className="grid-item-drivers">
-            <Img fluid={data.Verstappen.childImageSharp.fluid} />
-            <p>Max Verstappen</p>
-          </Link>
-          <Link to="/Albon" className="grid-item-drivers">
-            <Img fluid={data.Albon.childImageSharp.fluid} />
-            <p>Alexander Albon</p>
-          </Link>
-        </div>
-        <div id="rest">
-          <Link to="/Gasly" className="grid-item-drivers">
-            <Img fluid={data.Gasly.childImageSharp.fluid} />
-            <p>Pierre Gasly</p>
-          </Link>
-          <Link to="/Giovinazzi" className="grid-item-drivers">
-            <Img fluid={data.Giovinazzi.childImageSharp.fluid} />
-            <p>Antonio Giovinazzi</p>
-          </Link>
-          <Link to="/Grosjean" className="grid-item-drivers">
-            <Img fluid={data.Grosjean.childImageSharp.fluid} />
-            <p>Romain Grosjean</p>
-          </Link>
-          <Link to="/Hulkenberg" className="grid-item-drivers">
-            <Img fluid={data.Hulkenberg.childImageSharp.fluid} />
-            <p>Nico Hulkenberg</p>
-          </Link>
-          <Link to="/Kubica" className="grid-item-drivers">
-            <Img fluid={data.Kubica.childImageSharp.fluid} />
-            <p>Robert Kubica</p>
-          </Link>
-          <Link to="/Kvyat" className="grid-item-drivers">
-            <Img fluid={data.Kvyat.childImageSharp.fluid} />
-            <p>Daniil Kvyat</p>
-          </Link>
-
-          <Link to="/Magnussen" className="grid-item-drivers">
-            <Img fluid={data.Magnussen.childImageSharp.fluid} />
-            <p>Kevin Magnussen</p>
-          </Link>
-          <Link to="/Norris" className="grid-item-drivers">
-            <Img fluid={data.Norris.childImageSharp.fluid} />
-            <p>Lando Norris</p>
-          </Link>
-          <Link to="/Perez" className="grid-item-drivers">
-            <Img fluid={data.Perez.childImageSharp.fluid} />
-            <p>Sergio Perez</p>
-          </Link>
-          <Link to="/Raikkonen" className="grid-item-drivers">
-            <Img fluid={data.Raikkonen.childImageSharp.fluid} />
-            <p>Kimi Räikkönen</p>
-          </Link>
-          <Link to="/Ricciardo" className="grid-item-drivers">
-            <Img fluid={data.Ricciardo.childImageSharp.fluid} />
-            <p>Daniel Ricciardo</p>
-          </Link>
-          <Link to="/Russell" className="grid-item-drivers">
-            <Img fluid={data.Russell.childImageSharp.fluid} />
-            <p>George Russell</p>
-          </Link>
-          <Link to="/Sainz" className="grid-item-drivers">
-            <Img fluid={data.Sainz.childImageSharp.fluid} />
-            <p>Carlos Sainz</p>
-          </Link>
-          <Link to="/Stroll" className="grid-item-drivers">
-            <Img fluid={data.Stroll.childImageSharp.fluid} />
-            <p>Lance Stroll</p>
-          </Link>
-        </div>
+        <Link to="/Hamilton" className="grid-item-drivers">
+          <Img fluid={data.Hamilton.childImageSharp.fluid} />
+          <p>Lewis Hamilton </p>
+        </Link>
+        <Link to="/Bottas" className="grid-item-drivers">
+          <Img fluid={data.Bottas.childImageSharp.fluid} />
+          <p>Valtteri Bottas</p>
+        </Link>
+        <Link to="/Vettel" className="grid-item-drivers">
+          <Img fluid={data.Vettel.childImageSharp.fluid} />
+          <p>Sebastian Vettel</p>
+        </Link>
+        <Link to="/Leclerc" className="grid-item-drivers">
+          <Img fluid={data.Leclerc.childImageSharp.fluid} />
+          <p>Charles Leclerc</p>
+        </Link>
+        <Link to="/Verstappen" className="grid-item-drivers">
+          <Img fluid={data.Verstappen.childImageSharp.fluid} />
+          <p>Max Verstappen</p>
+        </Link>
+        <Link to="/Albon" className="grid-item-drivers">
+          <Img fluid={data.Albon.childImageSharp.fluid} />
+          <p>Alexander Albon</p>
+        </Link>
+        <Link to="/Norris" className="grid-item-drivers">
+          <Img fluid={data.Norris.childImageSharp.fluid} />
+          <p>Lando Norris</p>
+        </Link>
+        <Link to="/Sainz" className="grid-item-drivers">
+          <Img fluid={data.Sainz.childImageSharp.fluid} />
+          <p>Carlos Sainz</p>
+        </Link>
+        <Link to="/Hulkenberg" className="grid-item-drivers">
+          <Img fluid={data.Hulkenberg.childImageSharp.fluid} />
+          <p>Nico Hulkenberg</p>
+        </Link>
+        <Link to="/Ricciardo" className="grid-item-drivers">
+          <Img fluid={data.Ricciardo.childImageSharp.fluid} />
+          <p>Daniel Ricciardo</p>
+        </Link>
+        <Link to="/Gasly" className="grid-item-drivers">
+          <Img fluid={data.Gasly.childImageSharp.fluid} />
+          <p>Pierre Gasly</p>
+        </Link>
+        <Link to="/Kvyat" className="grid-item-drivers">
+          <Img fluid={data.Kvyat.childImageSharp.fluid} />
+          <p>Daniil Kvyat</p>
+        </Link>
+        <Link to="/Perez" className="grid-item-drivers">
+          <Img fluid={data.Perez.childImageSharp.fluid} />
+          <p>Sergio Perez</p>
+        </Link>
+        <Link to="/Stroll" className="grid-item-drivers">
+          <Img fluid={data.Stroll.childImageSharp.fluid} />
+          <p>Lance Stroll</p>
+        </Link>
+        <Link to="/Giovinazzi" className="grid-item-drivers">
+          <Img fluid={data.Giovinazzi.childImageSharp.fluid} />
+          <p>Antonio Giovinazzi</p>
+        </Link>
+        <Link to="/Raikkonen" className="grid-item-drivers">
+          <Img fluid={data.Raikkonen.childImageSharp.fluid} />
+          <p>Kimi Räikkönen</p>
+        </Link>
+        <Link to="/Grosjean" className="grid-item-drivers">
+          <Img fluid={data.Grosjean.childImageSharp.fluid} />
+          <p>Romain Grosjean</p>
+        </Link>
+        <Link to="/Magnussen" className="grid-item-drivers">
+          <Img fluid={data.Magnussen.childImageSharp.fluid} />
+          <p>Kevin Magnussen</p>
+        </Link>
+        <Link to="/Kubica" className="grid-item-drivers">
+          <Img fluid={data.Kubica.childImageSharp.fluid} />
+          <p>Robert Kubica</p>
+        </Link>
+        <Link to="/Russell" className="grid-item-drivers">
+          <Img fluid={data.Russell.childImageSharp.fluid} />
+          <p>George Russell</p>
+        </Link>
       </div>
 
       <Swiper {...params} className="swiper-drivers-container">
-        <Link to="/Albon" className="swiper-drivers">
-          <Img fluid={data.Albon.childImageSharp.fluid} />
-          <p>Alexander Albon</p>
+        <Link to="/Hamilton" className="swiper-drivers">
+          <Img fluid={data.Hamilton.childImageSharp.fluid} />
+          <p>Lewis Hamilton </p>
         </Link>
         <Link to="/Bottas" className="swiper-drivers">
           <Img fluid={data.Bottas.childImageSharp.fluid} />
           <p>Valtteri Bottas</p>
         </Link>
-        <Link to="/Gasly" className="swiper-drivers">
-          <Img fluid={data.Gasly.childImageSharp.fluid} />
-          <p>Pierre Gasly</p>
-        </Link>
-        <Link to="/Giovinazzi" className="swiper-drivers">
-          <Img fluid={data.Giovinazzi.childImageSharp.fluid} />
-          <p>Antonio Giovinazzi</p>
-        </Link>
-        <Link to="/Grosjean" className="swiper-drivers">
-          <Img fluid={data.Grosjean.childImageSharp.fluid} />
-          <p>Romain Grosjean</p>
-        </Link>
-        <Link to="/Hamilton" className="swiper-drivers">
-          <Img fluid={data.Hamilton.childImageSharp.fluid} />
-          <p>Lewis Hamilton </p>
-        </Link>
-        <Link to="/Hulkenberg" className="swiper-drivers">
-          <Img fluid={data.Hulkenberg.childImageSharp.fluid} />
-          <p>Nico Hulkenberg</p>
-        </Link>
-        <Link to="/Kubica" className="swiper-drivers">
-          <Img fluid={data.Kubica.childImageSharp.fluid} />
-          <p>Robert Kubica</p>
-        </Link>
-        <Link to="/Kvyat" className="swiper-drivers">
-          <Img fluid={data.Kvyat.childImageSharp.fluid} />
-          <p>Daniil Kvyat</p>
+        <Link to="/Vettel" className="swiper-drivers">
+          <Img fluid={data.Vettel.childImageSharp.fluid} />
+          <p>Sebastian Vettel</p>
         </Link>
         <Link to="/Leclerc" className="swiper-drivers">
           <Img fluid={data.Leclerc.childImageSharp.fluid} />
           <p>Charles Leclerc</p>
         </Link>
-        <Link to="/Magnussen" className="swiper-drivers">
-          <Img fluid={data.Magnussen.childImageSharp.fluid} />
-          <p>Kevin Magnussen</p>
+        <Link to="/Verstappen" className="swiper-drivers">
+          <Img fluid={data.Verstappen.childImageSharp.fluid} />
+          <p>Max Verstappen</p>
+        </Link>
+        <Link to="/Albon" className="swiper-drivers">
+          <Img fluid={data.Albon.childImageSharp.fluid} />
+          <p>Alexander Albon</p>
         </Link>
         <Link to="/Norris" className="swiper-drivers">
           <Img fluid={data.Norris.childImageSharp.fluid} />
           <p>Lando Norris</p>
         </Link>
-        <Link to="/Perez" className="swiper-drivers">
-          <Img fluid={data.Perez.childImageSharp.fluid} />
-          <p>Sergio Perez</p>
+        <Link to="/Sainz" className="swiper-drivers">
+          <Img fluid={data.Sainz.childImageSharp.fluid} />
+          <p>Carlos Sainz</p>
         </Link>
-        <Link to="/Raikkonen" className="swiper-drivers">
-          <Img fluid={data.Raikkonen.childImageSharp.fluid} />
-          <p>Kimi Räikkönen</p>
+        <Link to="/Hulkenberg" className="swiper-drivers">
+          <Img fluid={data.Hulkenberg.childImageSharp.fluid} />
+          <p>Nico Hulkenberg</p>
         </Link>
         <Link to="/Ricciardo" className="swiper-drivers">
           <Img fluid={data.Ricciardo.childImageSharp.fluid} />
           <p>Daniel Ricciardo</p>
         </Link>
-        <Link to="/Russell" className="swiper-drivers">
-          <Img fluid={data.Russell.childImageSharp.fluid} />
-          <p>George Russell</p>
+        <Link to="/Gasly" className="swiper-drivers">
+          <Img fluid={data.Gasly.childImageSharp.fluid} />
+          <p>Pierre Gasly</p>
         </Link>
-        <Link to="/Sainz" className="swiper-drivers">
-          <Img fluid={data.Sainz.childImageSharp.fluid} />
-          <p>Carlos Sainz</p>
+        <Link to="/Kvyat" className="swiper-drivers">
+          <Img fluid={data.Kvyat.childImageSharp.fluid} />
+          <p>Daniil Kvyat</p>
+        </Link>
+        <Link to="/Perez" className="swiper-drivers">
+          <Img fluid={data.Perez.childImageSharp.fluid} />
+          <p>Sergio Perez</p>
         </Link>
         <Link to="/Stroll" className="swiper-drivers">
           <Img fluid={data.Stroll.childImageSharp.fluid} />
           <p>Lance Stroll</p>
         </Link>
-        <Link to="/Verstappen" className="swiper-drivers">
-          <Img fluid={data.Verstappen.childImageSharp.fluid} />
-          <p>Max Verstappen</p>
+        <Link to="/Giovinazzi" className="swiper-drivers">
+          <Img fluid={data.Giovinazzi.childImageSharp.fluid} />
+          <p>Antonio Giovinazzi</p>
         </Link>
-        <Link to="/Vettel" className="swiper-drivers">
-          <Img fluid={data.Vettel.childImageSharp.fluid} />
-          <p>Sebastian Vettel</p>
+        <Link to="/Raikkonen" className="swiper-drivers">
+          <Img fluid={data.Raikkonen.childImageSharp.fluid} />
+          <p>Kimi Räikkönen</p>
+        </Link>
+        <Link to="/Grosjean" className="swiper-drivers">
+          <Img fluid={data.Grosjean.childImageSharp.fluid} />
+          <p>Romain Grosjean</p>
+        </Link>
+        <Link to="/Magnussen" className="swiper-drivers">
+          <Img fluid={data.Magnussen.childImageSharp.fluid} />
+          <p>Kevin Magnussen</p>
+        </Link>
+        <Link to="/Kubica" className="swiper-drivers">
+          <Img fluid={data.Kubica.childImageSharp.fluid} />
+          <p>Robert Kubica</p>
+        </Link>
+        <Link to="/Russell" className="swiper-drivers">
+          <Img fluid={data.Russell.childImageSharp.fluid} />
+          <p>George Russell</p>
         </Link>
       </Swiper>
 
